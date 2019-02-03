c8d6a44ebc964311f334921dd873f4a29d9e87ff
Charles <ellerkaresz@gmail.com>
2019-02-03 20:28:42 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# gesture_app
+
+- Drag horizontally to change the gradient
+- Drag vertically to increase or decrease the border-radius on the rectangle
+- Double tap to reset the gradient
+- Long tap to switch between the rectangle and the circle
---
+++ lib/main.dart
@@ -0,0 +1,84 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(GestureApp());
+
+class GetHorizontalPixels extends StatefulWidget{
+      @override
+      _HorizontalPixelState createState() => _HorizontalPixelState();
+}
+
+class _HorizontalPixelState extends State<GetHorizontalPixels>{
+      double horizontalPixels = 0.0;
+      double verticalPixels = 0.0;
+      bool circle = true;
+
+      @override
+      Widget build(BuildContext context){
+            return GestureDetector(
+                  onHorizontalDragUpdate: (update) => setState(() => horizontalPixels = update.globalPosition.dx),
+                  onVerticalDragUpdate: (update) => setState(() => verticalPixels = update.globalPosition.dy),
+                  onDoubleTap: () => setState(() => horizontalPixels = 0),
+                  onLongPress: () => setState(() => circle = !circle),
+                  child: Transform(
+                        child: Container(
+                              child: Center(
+                                    child: Text('$horizontalPixels px'),
+                              ),
+                              decoration: BoxDecoration(
+                                    gradient: LinearGradient(
+                                          begin: Alignment(0.0, -1.0),
+                                          end: Alignment(horizontalPixels * 0.005, 0.6),
+                                          colors: [
+                                                Color.fromRGBO(66, 86, 244, 0.9),
+                                                Color.fromRGBO(229, 66, 244, 0.9),
+                                          ],
+                                    ),
+                                    boxShadow: [
+                                          BoxShadow(
+                                                blurRadius: 3,
+                                                offset: Offset(0, 0.5),
+                                          )
+                                    ],
+                                    shape: circle ? BoxShape.circle : BoxShape.rectangle,
+                                    borderRadius: circle ? null : BorderRadius.all(Radius.circular(verticalPixels * 0.2)),
+                              ),
+                              width: 300,
+                              height: 400,
+                        ),
+                        transform: Matrix4.identity()..rotateZ(-horizontalPixels * 0.005 * 3.14 / 180),
+                        alignment: FractionalOffset.center,
+                  ),
+            );
+      }
+}
+
+class GestureApp extends StatelessWidget{
+      @override
+
+      Widget build(BuildContext context){
+            TextStyle customTextStyle = TextStyle(
+                  color: Colors.grey[850],
+                  fontSize: 17.0,
+                  fontWeight: FontWeight.w300,
+            );
+            return MaterialApp(
+                  title: 'Gesture App',
+                  home: Scaffold(
+                        appBar: AppBar(
+                              title: RichText(
+                                    text: TextSpan(
+                                          style: customTextStyle,
+                                          children: [
+                                                TextSpan(text: 'Lorem'),
+                                                TextSpan(text: 'Ipsum', style: TextStyle(fontWeight: FontWeight.w900,)),
+                                          ]
+                                    ),
+                              ),
+                        ),
+                        body: Center(
+                              child: GetHorizontalPixels(),
+                        ),
+                  ),
+            );
+      }
+}
